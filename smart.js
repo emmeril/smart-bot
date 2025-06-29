5f869b1ce3681567dc8628b9903d96bb686b9ab7
itsmtt <ithelpdesk.smtt@gmail.com>
2025-06-29 11:50:18 +0700

    Increase maximum hold time from 720 to 1440 minutes

--- smart.js
+++ smart.js
@@ -36,7 +36,7 @@ const TP_PERCENT = 0.03;
 const SL_PERCENT = 0.02;
 const COOLDOWN_MINUTES = 30;
 const LOSS_LIMIT = 3;
-const MAX_HOLD_MINUTES = 720;
+const MAX_HOLD_MINUTES = 1440;
 
 const exchange = new ccxt.binance({
   apiKey: isBacktest ? undefined : process.env.API_KEY,
