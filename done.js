bcad39fe654489b25c470d59e299c7ca9b77d8b1
itsmtt <ithelpdesk.smtt@gmail.com>
2025-09-26 11:27:02 +0700

    coba

--- done.js
+++ done.js
@@ -838,8 +838,8 @@ console.log(`-----------------------------------`);
 
 // Detail Indikator
 console.log(`📝 Detail Indikator:`);
-console.log(`   - Crossover MA: ${isCrossedUp ? "⬆️\u00A0MA7 Crossed Up MA25" : isCrossedDown ? "⬇️\u00A0MA7 Crossed Down MA25" : "↔️\u00A0Tidak Ada"}`);
-console.log(`   - Posisi Harga vs MA99: ${isPriceAboveMA99 ? "⬆️\u00A0Harga di atas MA99 (Tren Naik)" : "⬇️\u00A0Harga di bawah MA99 (Tren Turun)"}`);
+console.log(`   - Crossover MA: ${isCrossedUp ? "⬆️\nMA7 Crossed Up MA25" : isCrossedDown ? "⬇️\nMA7 Crossed Down MA25" : "↔️\nTidak Ada"}`);
+console.log(`   - Posisi Harga vs MA99: ${isPriceAboveMA99 ? "⬆️\nHarga di atas MA99 (Tren Naik)" : "⬇️\nHarga di bawah MA99 (Tren Turun)"}`);
 
 // Informasi Harga
 console.log(`💰 Harga Saat Ini: ${formatPrice(price)}`);
